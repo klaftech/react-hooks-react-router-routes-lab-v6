fde6b3e9b7478238019e52d905a76dac61c80861
Elchonon <ekn400@gmail.com>
2024-12-02 16:13:10 -0500

    lab attempt

--- src/pages/Actors.jsx
+++ src/pages/Actors.jsx
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
+import NavBar from '../components/NavBar'
 
 function Actors() {
+  
+  const [actors,setActors] = useState()
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/actors")
+      .then(res => res.json())
+      .then(data => setActors(data))
+  },[])
+  
+  if(!actors) return (<div>Loading actors</div>)
+  
+  const actorData = actors.map(actor => {
+    return (<article key={actor.id}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((element) => <li key={element}>{element}</li>)}
+      </ul>
+    </article>)
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Actor info here! */}
+        <h1>Actors Page</h1>
+        {actorData}
       </main>
     </>
   );
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
+import {useParams} from 'react-router-dom';
+import NavBar from '../components/NavBar'
 
 function Movie() {
-  return (
+  const params = useParams()
+  const [movie,setMovie] = useState()
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/movies/"+params.id)
+    .then(res => res.json())
+    .then(data => setMovie(data))
+  },[])
+
+  if(!movie) return (<div>Movie Loading...</div>)
+  
+    return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        {movie.genres.map((element) => <span key={element}>{element}</span>)}
       </main>
     </>
   );
